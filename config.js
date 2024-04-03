0b4ba6431e648b96220aaf4861dccc2885cb8c8a
drogva <156391770+drogva@users.noreply.github.com>
2024-04-04 08:23:16 +0900

    Update config.js

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ document.addEventListener("DOMContentLoaded", function() {
 
 function fetchBackendData() {
     // 여기에 백엔드로 요청을 보내는 코드 작성
-    const API_BASE_URL =  'https://web.seunghobet.link';
+    const API_BASE_URL =  'https://www.seunghobet.link';
     const endpoint = '/api/super-shy'; // 클릭 시 요청할 엔드포인트를 변경
 
     fetch(API_BASE_URL + endpoint)
